e1d234bbb79cfa5ac2ab3e0ff23552af93351f1c
fabian-flassig <64731310+fabian-flassig@users.noreply.github.com>
2025-07-26 11:05:40 +0200

    Update image_fading.js

--- script/image_fading.js
+++ script/image_fading.js
@@ -9,14 +9,15 @@ window.addEventListener('scroll', () => {
   const scrollTop = window.scrollY;
   const windowHeight = window.innerHeight;
   const maxScroll = windowHeight;
-  const opacity = Math.max(0, 1 - scrollTop / maxScroll);
+  const opacity = Math.max(0, 0.2 * (1 - scrollTop / maxScroll));
   overlay.style.opacity = opacity.toFixed(2);
 });
 
 // Flicker effect logic
 function startFlicker() {
   if (flickerInterval) return; // prevent duplicates
-  flicker.style.opacity = 1;
+   overlay.style.opacity = 1;
+  flicker.style.opacity = 0.1;
   flickerInterval = setInterval(() => {
     const randomOpacity = 0.1 + Math.random() * 0.1;
     flicker.style.opacity = randomOpacity.toFixed(2);
